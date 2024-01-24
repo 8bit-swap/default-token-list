dacfbf420580a96418767bf77216e4d65a7f649b
Kinco <colin.kindt@gmail.com>
2024-01-24 23:16:56 +0100

    Initial commit

---
+++ src/buildList.js
@@ -0,0 +1,31 @@
+const { version } = require("../package.json");
+const socotra = require("./tokens/socotra.json");
+
+const bridgeUtils = require('@uniswap/token-list-bridge-utils');
+
+module.exports = function buildList() {
+  const parsed = version.split(".");
+  const l1List = {
+    name: "8Bit-Swap Default",
+    timestamp: new Date().toISOString(),
+    version: {
+      major: +parsed[0],
+      minor: +parsed[1],
+      patch: +parsed[2],
+    },
+    tags: {},
+    logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir", // TODO change
+    keywords: ["8bit-swap", "default"],
+    tokens: [
+      ...socotra
+    ]
+      // sort them by symbol for easy readability
+      .sort((t1, t2) => {
+        if (t1.chainId === t2.chainId) {
+          return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
+        }
+        return t1.chainId < t2.chainId ? -1 : 1;
+      }),
+  };
+  return bridgeUtils.chainify(l1List);
+};
